a7c5cacf2280174b6273d8546bd88c8f20bbb29b
danny <fowlerd@cardiff.ac.uk>
2016-11-23 19:06:06 +0000

    added queries

---
+++ northwind_queries.sql
@@ -0,0 +1,16 @@
+use northwind;
+
+-- selects all citys from customers with no duplicates.
+SELECT DISTINT city FROM customers;
+
+-- finds email address of second customer
+SELECT email_address FROM customers WHERE id=2;
+
+-- displays total number of shippers
+SELECT COUNT(id) AS total_shippers FROM shippers;
+
+-- displays all job titles with no duplicates
+SELECT DISTINCT job_title FROM employees;
+
+--shows mobile number of employee with id 3
+SELECT mobile_phone FROM employees WHERE id=3;
